e02ffe1329a33c8f273c25fff3bf4f0dd49a45c9
Jyotiii Das <163004395+jyotidas4803@users.noreply.github.com>
2024-12-09 08:19:39 +0000

    initial setup with tabler icon

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Header from './components/Header'
+import Aside from './components/Aside'
+import Form from './components/Form'
+
+function App() {
+  return (
+    <div>App
+      <Header />
+      <Aside  />
+      <Form />
+
+    </div>
+  )
+}
+
+export default App
